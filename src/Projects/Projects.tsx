b7b189b336129e3ba268409b077bc07265a39c7b
Kirill <3331668@mail.ru>
2022-01-21 12:29:33 +0300

    change BrowserRouter to HashRouter

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -13,9 +13,9 @@ function Projects() {
 
                 <div className={css.project_block}>
 
-                    <Project title={'Account forms'} description={accountFormDescription} image={forms} link={''}/>
+                    <Project title={'Account forms'} description={accountFormDescription} image={forms} link={'http://5.53.124.33:3000/act_form'}/>
                     <Project title={'Social network'} description={socialNetworkDescription} image={socialnetwork} link={''}/>
-                    <Project title={'Todo list'} description={todoDescription} image={todo} link={''}/>
+                    <Project title={'Todo list'} description={todoDescription} image={todo} link={'https://3331668kirill.github.io/TodoList/'}/>
                     <Project title={'English words reminder'} description={englishDescription}
                              image={english} link={'https://3331668kirill.github.io/EnglishWordLearn/'}/>
 
